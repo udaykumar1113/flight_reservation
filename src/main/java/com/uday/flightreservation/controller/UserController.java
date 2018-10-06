27c0f2f05344492b2726c30830c81ed0b9cefbcd
udayk <udaykumar1113@gmail.com>
2018-10-06 16:28:54 -0400

    Added logging to controller with root level log

--- src/main/java/com/uday/flightreservation/controller/UserController.java
+++ src/main/java/com/uday/flightreservation/controller/UserController.java
@@ -2,6 +2,8 @@ package com.uday.flightreservation.controller;
 
 import com.uday.flightreservation.entities.User;
 import com.uday.flightreservation.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -15,6 +17,8 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(UserController.class);
 
     @RequestMapping("/showReg")
     public String showRegistration(){
@@ -31,15 +35,18 @@ public class UserController {
     public String login(@RequestParam("email") String email,
                         @RequestParam("password") String password,
                         ModelMap modelMap){
-
-        System.out.print("Inside login with "+email+" "+password);
+        LOGGER.info("Inside login with "+email+" "+password);
+        //System.out.print("Inside login with "+email+" "+password);
         if(userRepository.findByEmail(email).getEmail().equals(email)) {
-            System.out.print(userRepository.findByEmail(email)+" "+email);
+            //System.out.print(userRepository.findByEmail(email)+" "+email);
+            LOGGER.info("User able to login successful");
+            LOGGER.error("error test");
             modelMap.addAttribute("msg","successful login");
             return "findFlights";
         }
             else{
-            System.out.print(userRepository.findByEmail(email)+" "+email);
+            //System.out.print(userRepository.findByEmail(email)+" "+email);
+                LOGGER.error("login failed");
                 modelMap.addAttribute("msg","failure login");
         }
         return "login/login";
